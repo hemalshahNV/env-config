1dd9d83c5f6f5f64cc1705872f2ad734da77871c
Hemal Shah <hemalhshah@gmail.com>
2020-01-15 21:12:56 -0800

    Add script to setup required packages.

---
+++ install_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -ex
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    git \
+    curl \
+    tmux \
+    vim
+
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
+newgrp docker
+
